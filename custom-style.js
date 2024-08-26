5801b6430757a6f75db262277571f3a5f764286c
Sumu154 <sumaiyatasnim5548@gmail.com>
2024-08-26 23:57:33 +0600

    responsive nav bar is created with tailwind css

---
+++ custom-style.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+      extend: {
+
+        fontFamily: {
+          'Manrope': ['Manrope', 'sans-serif'],
+        },
+        
+        colors: {
+          green: '#ABEF5F',
+          lightgreen: 'rgba(171, 239, 95, 0.50)',
+          black: '#1C1C1C',
+          gray1: '#737373',
+          hover: '#494949',
+          ash: 'E9E9E9',
+          heading: '212121',
+        },
+      }
+    }
+  }
